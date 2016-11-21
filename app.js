381dbb969532391e3016b014895f3a994987b8a1
Vladimir Myskov <jaromudr@gmail.com>
2016-11-22 00:14:37 +0200

    Take documentation from docs folder

--- app.js
+++ app.js
@@ -1,66 +1,79 @@
 (function (global, $, undefined) {
-	var $win = $(window),
-		$nav = $('.subnav'),
-		navTop = $('.subnav').length && $('.subnav').offset().top - 40,
-		isFixed = 0,
-		processScroll = function () {
-			var i, scrollTop = $win.scrollTop()
-			if (scrollTop >= navTop && !isFixed) {
-				isFixed = 1;
-				$nav.addClass('subnav-fixed');
-			} else if (scrollTop <= navTop && isFixed) {
-				isFixed = 0;
-				$nav.removeClass('subnav-fixed');
-			}
-		};
+    var $win = $(window),
+        $nav = $('.subnav'),
+        navTop = $('.subnav').length && $('.subnav').offset().top - 40,
+        isFixed = 0,
+        processScroll = function () {
+            var i, scrollTop = $win.scrollTop()
+            if (scrollTop >= navTop && !isFixed) {
+                isFixed = 1;
+                $nav.addClass('subnav-fixed');
+            } else if (scrollTop <= navTop && isFixed) {
+                isFixed = 0;
+                $nav.removeClass('subnav-fixed');
+            }
+        };
 
-	$nav.on('click', function () {
-		if (!isFixed) setTimeout(function () {
-			$win.scrollTop($win.scrollTop() - 47);
-		}, 10)
-	})
-	$win.on('scroll', processScroll);
-	processScroll();
+    $nav.on('click', function () {
+        if (!isFixed) setTimeout(function () {
+            $win.scrollTop($win.scrollTop() - 47);
+        }, 10)
+    })
+    $win.on('scroll', processScroll);
+    processScroll();
 
-//	window.prettyPrint && prettyPrint()
+//  window.prettyPrint && prettyPrint()
 })(window, jQuery);
 
 
 (function (global, $, undefined) {
-	var $win = $(window),
-		$demoRows = $('.demo-row'),
-		template = $("#codeViewTemplate")[0].innerHTML,
-		removeTab = function(text){
-			var reg=/^(\t+)\S+.*/gmi,match,dTabCount=99999;
-			while(match = reg.exec(text)){
-				dTabCount > match[1].length && (dTabCount = match[1].length);
-			}
-			return text.replace(new RegExp("^\\t{"+dTabCount+"}","gmi"),"").replace(/\t/gmi,"  ");
-		};
+    var $win = $(window),
+        $demoRows = $('.demo-row'),
+        template = $("#codeViewTemplate")[0].innerHTML,
+        removeTab = function(text){
+            var reg=/^(\t+)\S+.*/gmi,match,dTabCount=99999;
+            while(match = reg.exec(text)){
+                dTabCount > match[1].length && (dTabCount = match[1].length);
+            }
+            return text.replace(new RegExp("^\\t{"+dTabCount+"}","gmi"),"").replace(/\t/gmi,"  ");
+        };
 
-	$(".prettyprint").each(function(i, pre){
-		var text = $(pre).html();
-		$(pre).html(removeTab(text));
-	});
+    $(".prettyprint").each(function(i, pre){
+        var text = $(pre).html();
+        $(pre).html(removeTab(text));
+    });
 
-	$demoRows.each(function(i,row){
-		var $row = $(row),
-		$demo = $row.find('.demo-span'),
-		$demoHtml = $demo.find('.demo-html'),
-		$demoCss = $demo.find('style'),
-		$demoJs = $demo.find('script'),
-		$code = $row.find('.code-span'),
-		demoId = $demoHtml[0].id,
-		view = {
-			id: demoId,
-			html:removeTab($demoHtml[0].outerHTML),
-			css:removeTab($demoCss[0].innerHTML),
-			js:removeTab($demoJs[0].innerHTML)
-		},
-		output = Mustache.render(template, view);
-		$code.append(output);
+    $demoRows.each(function(i,row){
+        var $row = $(row),
+        $demo = $row.find('.demo-span'),
+        $demoHtml = $demo.find('.demo-html'),
+        $demoCss = $demo.find('style'),
+        $demoJs = $demo.find('script'),
+        $code = $row.find('.code-span'),
+        demoId = $demoHtml[0].id,
+        view = {
+            id: demoId,
+            html:removeTab($demoHtml[0].outerHTML),
+            css:removeTab($demoCss[0].innerHTML),
+            js:removeTab($demoJs[0].innerHTML)
+        },
+        output = Mustache.render(template, view);
+        $code.append(output);
+    });
 
-	});
+    var markdown = new showdown.Converter();
 
-	window.prettyPrint && prettyPrint()
+    $("[data-md]").each(function(i, mdElement){
+        $.ajax({ url: $(mdElement).data('md'), 
+            type: 'get', 
+            dataType: 'html',
+            async: false,
+            success: function(data) {
+                $(mdElement).html(markdown.makeHtml(data));
+                $(".language-javascript", mdElement).parent("pre").addClass("prettyprint linenums");
+            } 
+        });
+    });
+
+    window.prettyPrint && prettyPrint()
 })(window, jQuery);
